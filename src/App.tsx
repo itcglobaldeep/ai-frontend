3def785044db086c5dfa381c058b4f696134dca5
itcglobaldeep <itcglobal1924@gmail.com>
2025-04-17 15:04:55 +0530

    ✅ Removed hardcoded OpenAI key, now using VITE env variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Toaster } from "sonner";
-import VoiceMic from "./components/voicechat/VoiceMic";
-
+import VoiceMic from "./components/voicechat/MicInput";
 
 function App() {
   return (
     <>
       <Toaster position="top-center" richColors />
-      <VoiceMic />
+      <MicInput />
     </>
   );
 }
